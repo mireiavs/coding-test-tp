8a5c2b9ecf244fcf8d977eb1e60af76eb7734a81
mireiavs <mvilalta85@gmail.com>
2019-05-29 10:06:21 +0200

    css fixes

--- client/src/components/SearchForm.js
+++ client/src/components/SearchForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-import { fetchEvents } from "../actions/eventActions";
+import { fetchEvents } from "../actions";
 
 class SearchForm extends Component {
   constructor(props) {
